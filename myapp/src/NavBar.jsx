4c8644757e993912b5e5a6dc15b72708943cda8f
devin-w-rogers <devin.w.rogers@gmail.com>
2023-12-15 00:24:57 -0600

    add better hexagon headshot, reorder/resize nav bar

--- myapp/src/NavBar.jsx
+++ myapp/src/NavBar.jsx
@@ -6,9 +6,9 @@ export function NavBar() {
             <img src={myLogo} alt="my-logo" />
             <i class='bx bx-menu-alt-right' id='menu-icon'></i>
             <a href="contact">Contact</a>
-            <a href="resume">Resume</a>
             <a href="work">Work</a>
             <a href="about">About Me</a>
+            <a href="home">Home</a>
         </nav>
     );
 }
--- myapp/src/homePage.jsx
+++ myapp/src/homePage.jsx
@@ -1,20 +1,17 @@
 import headshot from "./headshot.jpg"
+import hexaheadshot from "./hexa-headshot.png"
 
 
 export function HomePage() {
     return(
         <main>
             <div class="container">
-                <div class="hexagon">
-                    <div class="shape">
-                        <img src={headshot} alt="headshot" /> 
-                    </div>
-                </div>
+                <img src={hexaheadshot} class="hexhs" alt="headshot" /> 
             </div>
             <div class="container">
                 <div class="homepageText">
                     <h1>Devin Wolf Rogers</h1>
-                    <p style={{ fontSize: '5vh' }}>Hello there! Welcome to my website.</p>
+                    <p style={{ fontSize: '5vh' }}>Hello there!</p>
                     <p>I'm currently a senior Computer Science student at Carthage College and will be
                         graduating in May of 2024. I am looking forward to you browsing my website!
                     </p>
