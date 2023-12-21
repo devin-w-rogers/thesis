546c7444fd8a97a7173c4f6ace115f888ec76654
devin-w-rogers <devin.w.rogers@gmail.com>
2023-12-21 01:12:21 -0600

    add simple social media buttons, clean up some html code

--- myapp/src/homePage.jsx
+++ myapp/src/homePage.jsx
@@ -8,14 +8,16 @@ export function HomePage() {
                 <img src={hexaheadshot} class="hexhs" alt="headshot" /> 
             </div>
             <div class="container">
-                <div class="homepageText">
-                    <p style={{ fontSize: '5vh', marginBottom: '.3rem'}}>Hi! I'm</p>
-                    <h1>Devin Wolf Rogers.</h1>
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia,
-                    vitae aliquam nihil saepe quibusdam ex velit, quaerat doloribus laboriosam 
-                    fugiat id deserunt ratione perferendis possimus excepturi sit explicabo voluptatum!
-                    </p>
+                <p style={{ fontSize: '5vh', marginBottom: '.3rem'}}>Hi! I'm</p>
+                <h1>Devin Wolf Rogers.</h1>
+                <p style={{ marginLeft: '1rem', marginRight: '1rem'}}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse mollitia,
+                vitae aliquam nihil saepe quibusdam ex velit, quaerat doloribus laboriosam 
+                fugiat id deserunt ratione perferendis possimus excepturi sit explicabo voluptatum!
+                </p>
+                <div class="social">
+                    <a href="https://www.linkedin.com/in/devinwolfrogers" target="_blank" rel="noopener"><i class='bx bxl-linkedin'></i></a>
+                    <a href="https://github.com/devin-w-rogers" target="_blank" rel="noopener"><i class='bx bxl-github' ></i></a>
                 </div>
             </div>
         </main>
