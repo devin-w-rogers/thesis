2b8d1fc782c1a58e8de57bf1dc7abc30e667c145
devin-w-rogers <devin.w.rogers@gmail.com>
2024-03-19 10:50:49 -0500

    start adding deployment to minikube
    deployment not working at this stage

--- myapp/src/NavBar.jsx
+++ myapp/src/NavBar.jsx
@@ -6,7 +6,7 @@ export function NavBar() {
             <img src={myLogo} alt="my-logo" />
             <i class='bx bx-menu-alt-right' id='menu-icon'></i>
             <a href="contact">Contact</a>
-            <a href="work">Work</a>
+            <a href="projects">Projects</a>
             <a href="#aboutMe">About Me</a>
             <a href="home">Home</a>
         </nav>
