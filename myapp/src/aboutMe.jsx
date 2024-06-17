90eabbc52efe8df1931329954366a988499058c6
devin-w-rogers <98185051+devin-w-rogers@users.noreply.github.com>
2024-06-17 17:06:24 -0500

    give more space to buttons on home page

--- myapp/src/aboutMe.jsx
+++ myapp/src/aboutMe.jsx
@@ -2,8 +2,8 @@ import hexaheadshot from "./hexa-headshot.png"
 
 export function AboutMe() {
     return(
-        <body id="aboutMe">
-            <p id="aboutText"> Having just recently graduated from Carthage College, I am really looking forward to applying
+        <body class="aboutMe" id="aboutMe">
+            <p class="aboutText" id="aboutText"> Having recently graduated from Carthage College, I am looking forward to applying
                 my skills in a real work setting. I have experience with traditional software development and web development, 
                 primarily on the front-end. I have worked in an agile environment during my internship 
                 at General Dynamics Mission Systems with the DevSecOps team, utilizing user stories (Jira) and version control (Git). 
--- myapp/src/homePage.jsx
+++ myapp/src/homePage.jsx
@@ -9,9 +9,9 @@ export function HomePage() {
                 <img src={hexaheadshot} class="hexhs animate__animated animate__fadeInLeft" alt="headshot" /> 
             </div>
             <div class="container animate__animated animate__fadeInUp">
-                <p style={{ fontSize: '5vh', marginBottom: '.3rem'}}>Hello! My name is</p>
+                <h2>Hello! My name is</h2>
                 <h1>Devin Wolf Rogers.</h1>
-                <p style={{ marginLeft: '1rem', marginRight: '1rem'}}>
+                <p style={{ marginLeft: '1vw', marginRight: '1vw', lineHeight: '1.5'}}>
                     I am a recent graduate from Carthage College with a BA in Computer Science and am looking forward to joining the workforce! Thanks for looking at my website!
                 </p>
                 <div class="social">
