243e1f274429d39284602f27d9419e9efd935a6b
devin-w-rogers <98185051+devin-w-rogers@users.noreply.github.com>
2024-06-20 19:49:06 -0500

    got a rough mobile nav bar working

--- myapp/src/NavBar.jsx
+++ myapp/src/NavBar.jsx
@@ -1,15 +1,20 @@
 import myLogo from "./myLogo.png"
+import './Script.jsx';
 
 export function NavBar() {
     return(
-        <nav>
-            <img src={myLogo} alt="my-logo" />
+        <header>
+            <img src={myLogo} alt="my-logo" class="logo"/>
+
             <i class='bx bx-menu' id="menuIcon"></i>
-            <a href="#contact">Contact</a>
-            <a href="#work">Projects</a>
-            <a href="#aboutMe">About Me</a>
-            <a href="#home">Home</a>
-        </nav>
+
+            <nav class="navbar">
+                <a href="#home">Home</a>
+                <a href="#aboutMe">About Me</a>
+                <a href="#work">Projects</a>
+                <a href="#contact">Contact</a>
+            </nav>
+        </header>
     );
 }
 
--- myapp/src/Script.jsx
+++ myapp/src/Script.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+
+/* Change icon in nav bar -------------------------------------------------------  */
+let menuIcon = document.querySelector('#menuIcon');
+let nav = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+};
+
+/* Opening resume button --------------------------------------------------------  */
 export function openResume() {
     const pdfUrl = process.env.PUBLIC_URL + '/Devin_Rogers_CV.pdf';
     window.open(pdfUrl, '_blank', 'noopener');
