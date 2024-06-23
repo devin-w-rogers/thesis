b494880ba187a865e1f8a0e9e9898a0201d3eb4c
devin-w-rogers <98185051+devin-w-rogers@users.noreply.github.com>
2024-06-22 22:54:50 -0500

    fix margin on about page

--- myapp/src/aboutMe.jsx
+++ myapp/src/aboutMe.jsx
@@ -8,7 +8,7 @@ export function AboutMe() {
                 primarily on the front-end. I have worked in an agile environment during my internship 
                 at General Dynamics Mission Systems with the DevSecOps team, utilizing user stories (Jira) and version control (Git). 
                 I am a quick learner because I genuinely enjoy improving myself in every way possible.</p>
-            <div style={{paddingTop: '40px'}}>
+            <div style={{marginBottom: '10vh'}}>
                 <img src={hexaheadshot} class="hexhs" id="aboutHexHS" alt="headshot" /> 
             </div>
         </body>
