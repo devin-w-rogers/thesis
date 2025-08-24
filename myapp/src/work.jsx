a348b33fa12b7b9207d0bae6423908c613e96ed6
Devin Rogers <devin.w.rogers@gmail.com>
2025-08-23 23:39:25 -0500

    make projects section mobile responsive

--- myapp/src/work.jsx
+++ myapp/src/work.jsx
@@ -1,42 +1,37 @@
-import work1 from "./work1.jpg"
-import work2 from "./work2.jpg"
-import work3 from "./work3.jpg"
-
-
 export function Work() {
     return(
-        <section class="work" id="work">
-            <h2 class="heading">Some <span>Projects</span> </h2>
+        <section className="work" id="work">
+            <h2 className="heading">Some <span>Projects</span> </h2>
 
-            <div class="work-container">
-                <div class="card">
-                    <div class="inCard">
+            <div className="work-container">
+                <div className="card">
+                    <div className="inCard">
                         <h3>Project Name</h3>
                         <p>This project does this and this and that. 
                             It used this technology and that one too.
                             I really learned how to do this and that, 
                             and this also did something for that. </p>
-                        <button>Check it out!<i class='bx bx-link-external'></i></button>
+                        <button>Check it out!<i className='bx bx-link-external'></i></button>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="inCard">
+                <div className="card">
+                    <div className="inCard">
                         <h3>Project Name</h3>
                         <p>This project does this and this and that. 
                             It used this technology and that one too.
                             I really learned how to do this and that, 
                             and this also did something for that. </p>
-                        <button>Check it out!<i class='bx bx-link-external'></i></button>
+                        <button>Check it out!<i className='bx bx-link-external'></i></button>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="inCard">
+                <div className="card">
+                    <div className="inCard">
                         <h3>Project Name</h3>
                         <p>This project does this and this and that. 
                             It used this technology and that one too.
                             I really learned how to do this and that, 
                             and this also did something for that. </p>
-                        <button>Check it out!<i class='bx bx-link-external'></i></button>
+                        <button>Check it out!<i className='bx bx-link-external'></i></button>
                     </div>
                 </div>
             </div>
